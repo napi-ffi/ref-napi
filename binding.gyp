88671fdba6d636046f80ccd2795c18b5d57ec01b
Gilson Urbano <guf@hotmail.com.br>
2025-10-25 17:25:50 +0200

    Resurrect project

--- binding.gyp
+++ binding.gyp
@@ -16,6 +16,7 @@
       'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
       'CLANG_CXX_LIBRARY': 'libc++',
       'MACOSX_DEPLOYMENT_TARGET': '10.7',
+      'OTHER_CPLUSPLUSFLAGS' : [ '-std=c++11', '-stdlib=libc++' ],
     },
     'msvs_settings': {
       'VCCLCompilerTool': { 'ExceptionHandling': 1 },
--- src/binding.cc
+++ src/binding.cc
@@ -140,8 +140,10 @@ class InstanceData final : public RefNapi::Instance {
     if (it != pointer_to_orig_buffer.end())
       ab = it->second.ab.Value();
 
-    if (ab.IsEmpty()) {
-      length = std::max<size_t>(length, kMaxLength);
+    if (ab.IsEmpty() || (ab.ByteLength() < length)) {
+      // this ALWAYS creates an arraybuffer of 1Gbyte?
+      //length = std::max<size_t>(length, kMaxLength);
+      length = std::min<size_t>(length, kMaxLength);
       ab = Buffer<char>::New(env, ptr, length, [this](Env env, char* ptr) {
         UnregisterArrayBuffer(ptr);
       }).ArrayBuffer();
@@ -166,8 +168,15 @@ class InstanceData final : public RefNapi::Instance {
  */
 
 Value WrapPointer(Env env, char* ptr, size_t length) {
-  if (ptr == nullptr)
+  if (ptr == nullptr) {
     length = 0;
+  } else if (length == 0) {
+    // If length is 0 N-API doesn't guarantee it will save/restore ptr normally.
+    // For example, see https://nodejs.org/api/n-api.html#napi_get_typedarray_info
+    // "[out] data: ... If the length of the array is 0, this may be NULL or any
+    // other pointer value."
+    length = 1;
+  }
 
   InstanceData* data;
   if (ptr != nullptr && (data = InstanceData::Get(env)) != nullptr) {
